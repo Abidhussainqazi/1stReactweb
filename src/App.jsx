ae143126cb6f70d9f4790fbaf0ad1af5bfd8202c
Abidhussainqazi <abid.hussainqazi1991@gmail.com>
2023-07-17 14:14:08 +0500

    Second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 
-import Register from "./pages/register/Register";
-// import Home  from "./pages/home/Home";
+// import Register from "./pages/register/Register";
+import Home  from "./pages/home/Home";
 // import Watch  from "./pages/watch/Watch";
 import "./App.scss";
 
 const App = () => {
-  return <Register />;
+  return <Home />;
 };
 
 export default App;
