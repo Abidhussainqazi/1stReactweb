65c0362bc4e133cd6b303d844280d9116b64339b
Abidhussainqazi <abid.hussainqazi1991@gmail.com>
2023-07-19 00:56:27 +0500

    recent change

--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -1,31 +1,50 @@
-import React from 'react'
-import './register.scss'
+import { useRef } from "react";
+import { useState } from "react";
+import "./register.scss";
 
 const Register = () => {
-    return (
-        <div className='register'>
-            <div className="top">
-                <div className="wrapper">
-                    <img
-                        className='logo'
-                        src="https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png" alt="netflix logo"
-                    />
-                    <button className='loginButton'> Sign In</button>
-                </div>
-                </div>
-                <div className="container">
-                    <h1> Unlimited shows and movies</h1>
-                    <h2>Watch anywher. Cancle any time </h2>
-                    <p>
-                        ready to watch. enter your email to create or restart your membership
-                    </p>
-                    <div className="input">
-                        <input type="email" placeholder='email address' />
-                        <button className='regiterButton'>Get Started </button>
-                    </div>
-                </div>
-            </div>
-            )
-}
+  const [email, setEmail] = useState("");
 
-            export default Register
+  const emailRef = useRef();
+
+  const handleStart = () => {
+    setEmail(emailRef.current.value);
+  };
+
+  return (
+    <div className="register">
+      <div className="top">
+        <div className="wrapper">
+          <img
+            className="logo"
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/2560px-Netflix_2015_logo.svg.png"
+            alt=""
+          />
+          <button className="loginButton">Sign In</button>
+        </div>
+      </div>
+      <div className="container">
+        <h1>Unlimited movies, TV shows, and more.</h1>
+        <h2>Watch anywhere. Cancel anytime.</h2>
+        <p>
+          Ready to watch? Enter your email to create or restart your membership.
+        </p>
+        {!email ? (
+          <div className="input">
+            <input type="email" placeholder="email address" ref={emailRef} />
+            <button className="registerButton" onClick={handleStart}>
+              Get Started
+            </button>
+          </div>
+        ) : (
+          <div className="input">
+            <input type="password" placeholder="password" />
+            <button className="registerButton">Start</button>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Register;
