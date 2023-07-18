ab1bf32d2b6a6718ee8e8670f092bc0aba22782c
Abidhussainqazi <abid.hussainqazi1991@gmail.com>
2023-07-19 02:22:07 +0500

    recent change

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,25 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
 
 import Register from "./pages/register/Register";
-// import Home  from "./pages/home/Home";
-// import Watch  from "./pages/watch/Watch";
+import Home from "./pages/home/Home";
+import Watch from "./pages/watch/Watch";
+import Login from "./pages/login/Login";
+
 import "./App.scss";
 
 const App = () => {
-  return <Register/>;
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/register" component={Register} />
+        <Route exact path="/" component={Home} />
+        <Route exact path="/watch" component={Watch} />
+        <Route exact path="/login" component={Login} />
+      </Switch>
+    </Router>
+  );
 };
 
 export default App;
- 
--- src/component/Item/Item.jsx
+++ src/component/Item/Item.jsx
@@ -14,7 +14,7 @@ export default function ListItem({ index }) {
       onMouseLeave={() => setIsHovered(false)}
     >
       <img
-        src="https://occ-0-1723-92.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABU7D36jL6KiLG1xI8Xg_cZK-hYQj1L8yRxbQuB0rcLCnAk8AhEK5EM83QI71bRHUm0qOYxonD88gaThgDaPu7NuUfRg.jpg?r=4ee"
+        src="https://platinumlist.net/guide/wp-content/uploads/2023/03/8359_img_worlds_of_adventure-big1613913137.jpg-1024x683.webp"
         alt=""
       />
       {isHovered && (
